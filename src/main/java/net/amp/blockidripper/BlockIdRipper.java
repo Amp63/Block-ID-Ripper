6ede65ebff1c42448b67b9b7cac9b81d74c8d30c
Amp63 <milesburkart2299@gmail.com>
2023-06-27 22:18:11 -0400

    gets data from registries now and additionally rips item ids, enchants, and entities

--- src/main/java/net/amp/blockidripper/BlockIdRipper.java
+++ src/main/java/net/amp/blockidripper/BlockIdRipper.java
@@ -2,10 +2,9 @@ package net.amp.blockidripper;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import static net.minecraft.server.command.CommandManager.*;
 
-import net.minecraft.util.math.*;
-import net.minecraft.server.world.*;
-import net.minecraft.block.*;
-import net.minecraft.text.*;
+import net.minecraft.registry.Registries;
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
 
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.server.command.ServerCommandSource;
@@ -18,54 +17,62 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.File;
-import java.util.HashSet;
+import java.nio.file.Path;
 
 public class BlockIdRipper implements ModInitializer {
 	public static final String BLOCKIDRIPPER = "blockidripper";
 	public static final Logger LOGGER = LoggerFactory.getLogger(BLOCKIDRIPPER);
-
 	@Override
 	public void onInitialize() {
 		LOGGER.info("yo");
-
-		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("rip")
+		CommandRegistrationCallback.EVENT.register((dispatcher, registryAccess, environment) -> dispatcher.register(literal("ripid")
 				.executes(context -> {
 					ServerCommandSource source = context.getSource();
-					ServerWorld world = source.getWorld();
-
-					HashSet<String> seenBlocks = new HashSet<>();
-					String textFilePath = FabricLoader.getInstance().getGameDir().resolve("ripped-block-ids.txt").toString();
+					Path gameDir = FabricLoader.getInstance().getGameDir();
+					String itemIdFilePath = gameDir.resolve("ripped-item-ids.txt").toString();
+					String blockIdFilePath = gameDir.resolve("ripped-block-ids.txt").toString();
+					String enchantsFilePath = gameDir.resolve("ripped-enchants.txt").toString();
+					String entitiesFilePath = gameDir.resolve("ripped-entity-types.txt").toString();
 					try {
-						new File(textFilePath).createNewFile();
+						new File(itemIdFilePath).createNewFile();
+						new File(blockIdFilePath).createNewFile();
+						new File(enchantsFilePath).createNewFile();
+						new File(entitiesFilePath).createNewFile();
 					} catch (IOException e) {
-						System.out.println("Error creating block id file.");
+						System.out.println("Error creating files.");
 						e.printStackTrace();
 					}
+
 					try {
-						PrintWriter pw = new PrintWriter(textFilePath);
-						int x = 1;
-						int z = 1;
-						while (x < 308) {  // magic values go brr (change this for later versions if necessary)
-							while (z < 310) {
-								BlockState b = world.getBlockState(new BlockPos(x, 70, z));
-								String blockId = b.getBlock().toString();
-								blockId = blockId.substring(16, blockId.length() - 1);
-								if (!seenBlocks.contains(blockId)) {
-									seenBlocks.add(blockId);
-									pw.println(blockId);
-								}
-								z += 2;
-							}
-							x += 2;
-							z = 1;
+						PrintWriter itemIdPw = new PrintWriter(itemIdFilePath);
+						PrintWriter blockIdPw = new PrintWriter(blockIdFilePath);
+						PrintWriter enchantsPw = new PrintWriter(enchantsFilePath);
+						PrintWriter entitiesPw = new PrintWriter(entitiesFilePath);
+
+						for (Identifier id : Registries.ITEM.getIds()) {
+							itemIdPw.println(id.toString());
 						}
-						pw.close();
+						for (Identifier id : Registries.BLOCK.getIds()) {
+							blockIdPw.println(id.toString());
+						}
+						for (Identifier id : Registries.ENCHANTMENT.getIds()) {
+							enchantsPw.println(id.toString());
+						}
+						for (Identifier id : Registries.ENTITY_TYPE.getIds()) {
+							entitiesPw.println(id.toString());
+						}
+
+						itemIdPw.close();
+						blockIdPw.close();
+						enchantsPw.close();
+						entitiesPw.close();
+						source.sendMessage(Text.literal("Successfully ripped item ids, block ids, enchantments, and entity types."));
 					} catch (FileNotFoundException e) {
 						System.out.println("File not found");
 						e.printStackTrace();
 					}
-					source.sendMessage(Text.literal("Successfully ripped ids to " + textFilePath));
 					return 1;
 				})));
+
 	}
 }
